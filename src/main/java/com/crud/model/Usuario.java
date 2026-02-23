d9101a2fb6399be6b0c8bcd14d7488810400bc41
thiago <thiagofelixso@gmail.com>
2026-02-23 09:55:39 -0300

    adicionando Spring Data + removendo classe de testes

--- src/main/java/com/crud/model/Usuario.java
+++ src/main/java/com/crud/model/Usuario.java
@@ -9,8 +9,7 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Usuario implements Serializable{
-
-    
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
